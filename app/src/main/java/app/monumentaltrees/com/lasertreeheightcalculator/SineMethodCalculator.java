f0014889a5ea1cb1932f2b7818e46de504eb35fc
nickmcummins <nickmcummins@gmail.com>
2016-10-01 19:09:19 -0700

    Handle input units plus laser rangefinder app icon

--- app/src/main/java/app/monumentaltrees/com/lasertreeheightcalculator/SineMethodCalculator.java
+++ app/src/main/java/app/monumentaltrees/com/lasertreeheightcalculator/SineMethodCalculator.java
@@ -1,7 +1,7 @@
 package app.monumentaltrees.com.lasertreeheightcalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,9 +9,11 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SineMethodCalculator extends AppCompatActivity {
+    private static final double METERS_TO_FEET = 3.28084;
+    private static final String DEFAULT_UNITS = "ft";
+
     Button calculateHeightBtn;
     EditText distanceToHeight;
-    Spinner unitsToHeight;
     EditText distanceToBase;
     Spinner unitsToBase;
     TextView calculatedHeight;
@@ -21,36 +23,42 @@ public class SineMethodCalculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sine_method_calculator);
 
-        distanceToHeight = (EditText)findViewById(R.id.distanceToTop);
-        unitsToHeight = (Spinner)findViewById(R.id.unitsToTop);
+        distanceToHeight = (EditText) findViewById(R.id.distanceToTop);
 
-        distanceToBase = (EditText)findViewById(R.id.distanceToBase);
-        unitsToBase = (Spinner)findViewById(R.id.unitsToBase);
+        distanceToBase = (EditText) findViewById(R.id.distanceToBase);
+        unitsToBase = (Spinner) findViewById(R.id.unitsToBase);
 
-        calculatedHeight = (TextView)findViewById(R.id.calculatedHeight);
-        calculateHeightBtn = (Button)findViewById(R.id.button);
+        calculatedHeight = (TextView) findViewById(R.id.calculatedHeight);
+        calculateHeightBtn = (Button) findViewById(R.id.button);
 
         calculateHeightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculatedHeight.setText(
-                        String.valueOf(calculateHeight(distanceToHeight.getText().toString(), distanceToBase.getText().toString()))
-                        //distanceToHeight.getText().toString() + "-" + distanceToBase.getText().toString()
-                );
+                double distanceToHeightNumeric = parseEditText(distanceToHeight, unitsToBase);
+                double distanceToBaseNumeric = parseEditText(distanceToBase, unitsToBase);
+
+                double calculatedHeightNumeric = calculateHeight(distanceToHeightNumeric, distanceToBaseNumeric);
+                calculatedHeight.setText(String.format("%s %s",
+                        calculatedHeightNumeric, DEFAULT_UNITS));
 
             }
         });
     }
 
-    private double calculateHeight(String distanceToHeight, String distanceToBase)
-    {
-        return calculateHeight(Double.parseDouble(distanceToBase), Double.parseDouble(distanceToBase));
+    private double parseEditText(EditText editText, Spinner unitsToBase) {
+        double value;
+        double unitConversionFactor = unitsToBase.getSelectedItem().toString().equals("m") ? METERS_TO_FEET : 1.0;
+        try {
+            value = Double.parseDouble(editText.getText().toString());
+        } catch (NumberFormatException e) {
+            value = 1.0;
+        }
+        return value * unitConversionFactor;
     }
 
-    private double calculateHeight(double distanceToHeight, double distanceToBase)
-    {
+    private double calculateHeight(double distanceToHeight, double distanceToBase) {
         return Math.sqrt(
-                Math.pow(distanceToHeight, 2) - Math.pow(distanceToBase, 2)
+                Math.pow(distanceToHeight, 2.0) - Math.pow(distanceToBase, 2.0)
         );
     }
 
